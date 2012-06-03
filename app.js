bf20a12e6c7ae9fa32fe8f9aeb297dfec70876f9
dan L <danlangford@gmail.com>
2012-06-03 21:51:58 +0000

    trying to get heroku running

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var http = require("http");
+var http = require('http');
 
 var app = http.createServer(function(req, res){
     res.writeHead( 200, {'Content-Type': 'text/plain'} );
-    res.end( 'Hello World\n');
+    res.end( 'Hello World\n (from app)');
 });
 
 app.listen(process.env.PORT);
